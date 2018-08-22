4c7ebd7db3674d5cab691d090b0883a2d0e48035
Manuel Pissanos <mpissano@gmail.com>
2018-08-22 14:23:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,28 +1,28 @@
-function forLoop(array) {
-    for(let  i = 0; i < 25; i++ ) {
-      if( i !== 1) {
-       array.push(`I am ${i} strange loops.`)
-      } else {
-       array.push(`I am ${i} strange loop.`)
-      }
-    } 
-    return array;
-  }
+// function forLoop(array) {
+//     for(let  i = 0; i < 25; i++ ) {
+//       if( i !== 1) {
+//       array.push(`I am ${i} strange loops.`)
+//       } else {
+//       array.push(`I am ${i} strange loop.`)
+//       }
+//     } 
+//     return array;
+//   }
 
-function whileLoop(n) {
-  while ( n > 0) {
-    console.log(n--)
-  }
-    return 'done';
-  }
+// function whileLoop(n) {
+//   while ( n > 0) {
+//     console.log(n--)
+//   }
+//     return 'done';
+//   }
 
 
 function doWhileLoop(array) {
   
-  var i = 0;
+  var i = array.length;
 
   function incrementVariable() {
-    i = i + 1;
+    i = i - 1;
   }
   
   do {
