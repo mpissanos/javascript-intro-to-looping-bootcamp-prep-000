62c1da26c8404d0df7b4df873ece1ffc60a0e5ba
Manuel Pissanos <mpissano@gmail.com>
2018-08-22 14:04:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -12,15 +12,8 @@ function forLoop(array) {
 function whileLoop(n) {
   while ( n > 0) {
     console.log(n--)
+  }
     return 'done';
   }
-}
 
-whileLoop(n)
-         counts down from n to 0:
 
-      AssertionError: expected { Spy, 1 call } to have been called 30 times butgot 1
-      + expected - actual
-
-      -1
-      +30
