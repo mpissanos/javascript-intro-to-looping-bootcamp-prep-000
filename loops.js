255c026daf0e29bcb1e91cfe6ca37b2a664d07e8
Manuel Pissanos <mpissano@gmail.com>
2018-08-22 13:44:11 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+function forLoop(array) {
+  var myArr = []
+  for(let  i = [0]; i < 25; i++ ) {
+    if( i !== 1) {
+     myArr.push("I am "+ i + " strange loops.")
+    } else {
+     myArr.push("I am 1 strange loop.")
+    }
+  } 
+  return myArr;
+}
+
+
+
