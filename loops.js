d13e6ead9d40c651cd4e414fafdb7a2998418a32
Manuel Pissanos <mpissano@gmail.com>
2018-08-22 13:50:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,11 @@
 function forLoop(array) {
-  var myArr = 0  
     for(let  i = 0; i < 25; i++ ) {
       if( i !== 1) {
-       myArr.push(`I am ${i}}strange loops.`)
+       array.push(`I am ${i}}strange loops.`)
       } else {
-       myArr.push(`I am ${i} strange loop.`)
+       array.push(`I am ${i} strange loop.`)
       }
     } 
-    return myArr;
+    return array;
   }
 
